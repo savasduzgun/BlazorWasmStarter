0db93098a1ea0b41af92e2f0207607a2651a9d98
savasduzgun <savasdzgn@gmail.com>
2024-07-13 10:59:18 +0300

    Save password için icon eklendi

---
+++ BlazorWasmStarter/Enums/CheckBoxActionType.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorWasmStarter.Enums
+{
+    public enum CheckBoxActionType
+    {
+        Numbers = 1,
+        Lowercase = 2,
+        Uppercase = 3,
+        SpecialChars = 4
+
+    }
+}
